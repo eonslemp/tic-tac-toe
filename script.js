e9e3a6fe87d8284421940e1b93e519c1e80df055
Eon Slemp <eonslemp@gmail.com>
2022-09-15 10:10:10 -0400

    all basic game functions working

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const cells = Array.from(document.getElementsByClassName('cell'))
-cellState = ["","","","","","","",""]
-// if(player == )
-// const player = ;
-// console.log(cells)
-// cells.forEach(element,  () => element.addEventListener('mouseenter', () => {
-//     element.classList.toggle("light-gray") )
-// }
-// forEach()
-// cells.addEventListener('mouseover', )\
+cellState = ["", "", "", "", "", "", "", "", ""];
+const statusMessage = document.getElementById("game-status")
+
 whoseMove = 'X'
 let playAs = null;
 let crosses = document.getElementById('crosses')
 // console.log(crosses)
+
+function restart(){
+    // this function will reset the game state
+    cellState = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach(cell =>{
+        cell.innerHTML = "";
+        
+    })
+    // resets the game status message bar
+    statusMessage.innerText = '';
+}
+
 function selectCrosses(){
     restart();
     if(crosses.style.backgroundColor == 'rgb(143, 206, 202)'){
         crosses.style.backgroundColor = 'white';
+        whoseMove = "X";
         playAs = null;
         // console.log(playAs)
     } else {
+        whoseMove = "X";
         playAs = 'X';
         crosses.style.backgroundColor = 'rgb(143, 206, 202)';
         noughts.style.backgroundColor = 'white';
@@ -32,8 +40,10 @@ function selectNoughts(){
     if(noughts.style.backgroundColor == 'rgb(143, 206, 202)'){
         noughts.style.backgroundColor = 'white'
         playAs = null;
+        whoseMove = "X";
     } else {
         playAs = 'O';
+        whoseMove = "X";
         noughts.style.backgroundColor = 'rgb(143, 206, 202)';
         crosses.style.backgroundColor = 'white';
         }
@@ -44,6 +54,8 @@ noughts.addEventListener('click', selectNoughts)
 
 // crosses = true;
 // console.log(crosses)
+
+
 
 
 var opponent = null;
@@ -71,48 +83,153 @@ function selectSkynet(){
         spongebob.style.backgroundColor = 'white';
         }
 }
-
+// adding event listeners to spongebob for interactivity
 const spongebob = document.getElementById('spongebob')
 spongebob.addEventListener('click', selectSpongebob)
-
+spongebob.addEventListener('mouseenter', () =>{
+    spongebob.style.backgroundColor = 'rgb(143, 206, 202)';
+})
+spongebob.addEventListener('mouseout', () =>{
+    spongebob.style.backgroundColor = 'white';
+})
+// adding event listeners for interactivity to skynet
 const skynet = document.getElementById('skynet')
 skynet.addEventListener('click', selectSkynet)
+skynet.addEventListener('mouseenter', () => {
+    skynet.style.backgroundColor = 'rgb(143, 206, 202)';
+})
+skynet.addEventListener('mouseout', () => {
+    skynet.style.backgroundColor = 'white';
+})
 
-function fillCell(cell){
-    // cell.innerText = playAs;
-    console.log(cell)
+
+
+
+
+
+
+function changeMover(){
+    // this ternary alternates mover from crosses and naughts
+    whoseMove = (whoseMove == "X") ? "O" : "X";
 }
-// console.log(cells)
-// cells.forEach( cell => {
-//     // console.log(cell)
-//     cell.addEventListener('click', fillCell(cell))
-// })
+
 
 cells.forEach( cell => {
-    // console.log(cell)
+    // this arrow fx will add game logic functionality to each game matrix cell
     cell.addEventListener('click', () => {
-        if(cell.innerHTML == ""){
+        // conditional verifies that the cell is not already marked and that 
+        // the player has chosen crosses or noughts
+        if(cell.innerHTML == "" && playAs != null){
+            // if above conditions are met this line 
+            // assigns the appropriate character to the cell
             cell.innerHTML = whoseMove;
-            changeMover()
-
-            cellState[Number(cell.getAttribute("id"))] = playAs;
-            // console.log(cellState)
+            // this line updates the cellstate variable to reflect the board
+            cellState[Number(cell.getAttribute("id"))] = whoseMove;
+            // checks status of the game
+            checkWin(cellState);
+            // alternates the mover
+            changeMover();
         } 
        
     })
 })
 
-function restart(){
-    cells.forEach(cell =>{
-        cell.innerHTML = "";
-        cellState = ["","","","","","","",""];
-    })
-}
 
-function changeMover(){
-    whoseMove = (whoseMove == "X") ? "O" : "X"
-}
 
-function checkWin(){
 
+
+
+
+// function checkWin(gameArr){
+//     const statusMessage = document.getElementById("game-status")
+//     const winner = (whoseMove == "O") ? 'NUAGHTS' : 'CROSSES';
+//     switch(gameArr){
+//         case (gameArr[0] === gameArr[1]) && (gameArr[1] === gameArr[2]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[3] === gameArr[4]) && (gameArr[4] === gameArr[5]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[6] === gameArr[7]) && (gameArr[7] === gameArr[8]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[0] === gameArr[3]) && (gameArr[3] === gameArr[6]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;  
+//         case (gameArr[1] === gameArr[4]) && (gameArr[4] === gameArr[7]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[2] === gameArr[5]) && (gameArr[5] === gameArr[8]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[2] === gameArr[4]) && (gameArr[4] === gameArr[6]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         case (gameArr[0] === gameArr[4]) && (gameArr[4] === gameArr[8]):
+//             statusMessage.innerText = `${winner} WINS!`;
+//             break;
+//         default:
+//             statusMessage.innerText = 'No winner';
+//     }
+
+// }
+// let x = "X";
+// let y = "X";
+// let z = "X"
+// console.log(x == y && y == z)
+// console.log("X" == "X" == "X")
+// console.log("X" == "X" && "X" == "X")
+
+// const test = ['X', 'O', 2, 3, 'X', 'O', 6, 7, 'X']
+// console.log(test[0] === test[4] && test[4] === test[8])
+
+
+// this array stores the cell indices relevant to the 
+// eight possible win conditions
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function checkWin(gameArr){
+    // this variable stores the value of the current mover to display in case of a win
+    const winner = (whoseMove == "O") ? 'NUAGHTS' : 'CROSSES';
+// for loop to check the game array against the various win conditions
+    for(let condition of winConditions){
+        // these variables store the value of the game array at the indices 
+        // relevant to the different win conditions
+        const cellA = gameArr[condition[0]];
+        const cellB = gameArr[condition[1]];
+        const cellC = gameArr[condition[2]];
+// this conditional verifies that at least one of the variables 
+// is not a default setting for comparison in the next conditional
+        if(cellA != ""){
+// conditional checks the values against each other 
+            if(cellA == cellB && cellB == cellC){
+
+                console.log("win")
+    // sets value of game status bar
+                statusMessage.innerText = `${winner} WIN!`;
+    // sets playAs to null and prevents any further 
+    // moves until one of the buttons is clicked
+                playAs = null;
+                return cellA, cellB, cellC
+            }
+        }
+    }
+// conditional checks if any values remain to be filled 
+// if not, and the above win conditions have not been observed then 
+// there is a tie
+    if(gameArr.includes("") == false){
+        console.log('tie')
+        statusMessage.innerText = 'No clear winner';
+    }
+
+ 
 }
